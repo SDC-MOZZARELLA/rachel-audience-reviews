3279ed19b01353f67524a6b4539d22780c46addf
Jonathan Yang <jonyang221@gmail.com>
2019-10-26 19:34:41 -0700

    Fixed styling and all components.

--- client/src/Components/FakeAudReviews.jsx
+++ client/src/Components/FakeAudReviews.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
 import SinglePost from './SinglePost.jsx';
+
 
 var AudReviewList = (props) => {
   return (
     <>
-      {props.someReviews.map((review) =>
-        <SinglePost item={review}/>
+      {props.someReviews.map((review, index) =>
+        <SinglePost item={review} key={index}/>
       )}
     </>
   )
