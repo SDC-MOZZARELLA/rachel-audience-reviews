8f733d68cabe10fef8383745b188a6bdea2d422b
RachNovo <r.novoselac@gmail.com>
2019-11-05 15:01:36 -0500

    state of completion

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const db = require('../database/db.js');
+const db = require('../database/mongo-database/mongo-db.js');
 
 const json = bodyParser.json();
 const app = express();
